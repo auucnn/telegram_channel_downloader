3d49c3e0b90c1553dfbad14f0fd886d9f8f7451c
auucnn <68440054+auucnn@users.noreply.github.com>
2023-11-30 00:56:57 +0800

    Add files via upload

--- telegram.py
+++ telegram.py
@@ -1,28 +1,31 @@
 from telethon import TelegramClient, events
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telethon.sync import TelegramClient
+from telegram.ext import Updater, CommandHandler, Filters
 import asyncio
 import os
 
 # 配置
-api_id = '18138452'  # Telegram API ID
-api_hash = '42f89bf956575caa848bd97b0e774fe9'  # Telegram API Hash
-bot_token = '5854334680:AAGJnVrAXVhRO3BnfBUza2vy_DtpmLtxw4I'  # Bot Token
-save_path = '/cc'  # 文件保存路径
+api_id = '18138452'  # 替换为您的Telegram API ID
+api_hash = '42f89bf956575caa848bd97b0e774fe9'  # 替换为您的Telegram API Hash
+bot_token = '5854334680:AAGJnVrAXVhRO3BnfBUza2vy_DtpmLtxw4I'  # 替换为您的Bot Token
+save_path = '/cc'  # 替换为保存文件的路径
 
-# Telegram 客户端初始化
-client = TelegramClient('session', api_id, api_hash)
+# 初始化Telegram客户端
+client = TelegramClient('session_name', api_id, api_hash)
 
+# 下载媒体文件的异步函数
 async def download_media(message):
     if message.photo or message.video:
         file_path = os.path.join(save_path, message.file.name)
         await message.download_media(file=file_path)
-        print(f"文件已下载：{file_path}")
+        print(f'文件已下载：{file_path}')
 
+# 处理新消息的事件
 @client.on(events.NewMessage)
 async def handler(event):
     await download_media(event.message)
 
-# Telegram Bot 处理器
+# Telegram机器人的命令处理器
 def start(update, context):
     update.message.reply_text('发送Telegram群组或频道链接。')
 
@@ -32,10 +35,12 @@ def handle_link(update, context):
     asyncio.create_task(client.get_entity(url))
     update.message.reply_text('开始监控频道或群组。')
 
+# 主函数，设置和运行Telegram机器人
 def main():
     updater = Updater(bot_token, use_context=True)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
+
+    dp.add_handler(CommandHandler('start', start))
     dp.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_link))
 
     updater.start_polling()
